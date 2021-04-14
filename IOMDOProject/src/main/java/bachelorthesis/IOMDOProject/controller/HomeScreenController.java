7685766079381266fcd0e37d2fa2b3a632eed56a
Stefanie Neuenschwander <stefanie.neuenschwander97@gmail.com>
2021-04-14 11:22:21 +0200

    Added Record Patient Data View and linked to Homescreen

---
+++ IOMDOProject/src/main/java/bachelorthesis/IOMDOProject/controller/HomeScreenController.java
@@ -0,0 +1,50 @@
+package bachelorthesis.IOMDOProject.controller;
+
+import java.io.IOException;
+import java.util.ResourceBundle;
+
+import bachelorthesis.IOMDOProject.Main;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ * 
+ * @author neues4
+ *
+ */
+
+public class HomeScreenController {
+
+	@FXML
+    private Button btnNewProtocol;
+
+    @FXML
+    private Button btnProtocolOverview;
+
+    @FXML
+    private Button btnCreateQuery;
+
+    @FXML
+    private Button btnExpandOntology;
+
+    @FXML
+    private Button BtnSettings;
+
+    @FXML
+    private Label lblLoggedInAs;
+
+    @FXML
+    private Label lblName;
+
+    @FXML
+    private Button btnLogout;
+    
+    
+    public void goToCreateNewProtocol(ActionEvent event) throws IOException {
+    	Main main = new Main();
+    	main.changeScene("RecordDocument_PatientData.fxml");
+    }
+	
+}
