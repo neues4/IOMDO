846036de6ffffcf15086472da6245f8f46b0926d
Stefanie Neuenschwander <stefanie.neuenschwander97@gmail.com>
2021-04-27 10:02:35 +0200

    edited the OntologyEditor class, added methods

--- IOMDOProject/src/main/java/bachelorthesis/IOMDOProject/model/OntologyEditor.java
+++ IOMDOProject/src/main/java/bachelorthesis/IOMDOProject/model/OntologyEditor.java
@@ -6,6 +6,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.StringWriter;
+import java.util.ArrayList;
 import java.util.Iterator;
 
 import org.apache.jena.ontology.DatatypeProperty;
@@ -21,72 +22,104 @@ import org.apache.jena.rdf.model.RDFNode;
 
 import bachelorthesis.IOMDOProject.Main;
 
+
+/**
+ * 
+ * @author neues4
+ * 
+ *
+ */
 public class OntologyEditor {
-	
-	public static void main(String[] args) {
-		
-		OntModel model = ModelFactory.createOntologyModel();
-		
-		
-		// read a File into Ontology Model
-		String fileName = "/Users/stefanie/Desktop/IOMO_18.owl";
+
+	private static OntModel ontModel;
+
+	/**
+	 * Constructor to create a new Ontology Editor.
+	 * @param filePath the local path to the Ontology File (.owl)
+	 */
+	public OntologyEditor(String filePath) {
+
+		ontModel = ModelFactory.createOntologyModel();
 		try {
-			File file = new File(fileName);
+			File file = new File(filePath);
 			FileReader reader = new FileReader(file);
-			model = ModelFactory.createOntologyModel(OntModelSpec.OWL_DL_MEM);
-			
-			model.read(reader, null);
-			
-			//model.write(System.out, "RDF/XML-ABBREV");
+			ontModel = ModelFactory.createOntologyModel(OntModelSpec.OWL_DL_MEM);
+			ontModel.read(reader, null);
+			//ontModel.write(System.out, "RDF/XML-ABBREV");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		// retrive ontology classes
-		/*
-		Iterator classIter = model.listClasses();
+	}
+
+
+	/**
+	 * Method to return all Classes present in the Ontology
+	 * (only to the console to check if the ontology was loaded)
+	 * @return 
+	 */
+	public String getAllOntologyClasses() {
+		Iterator classIter = ontModel.listClasses();
 		while (classIter.hasNext()) {
 			OntClass ontClass = (OntClass) classIter.next();
 			String uri = ontClass.getURI();
 			if (uri != null)
 				System.out.println(uri);
 		}
-		*/
-		
-		// retrive a specified class
-		/*
-		String classURI = "http://www.semanticweb.org/ontologies/2021/1/24/IOMO/IOMO_0000176";
-		OntClass test = model.getOntClass(classURI);
-		System.out.println(test);
-		*/
-		
-		// get the ontology URI
-		
-		/*
+		return null;
+	}
+
+
+	/**
+	 * Method to return the label of a specific class
+	 * @param classURI the class uri
+	 * @return the label of the class in english
+	 */
+	public String getOntologyClass(String classURI) {
+		OntClass ontClass = ontModel.getOntClass(classURI);
+		return ontClass.getLabel("EN");
+	}
+
+
+	/**
+	 * Method to return the URI of the loaded ontology
+	 * @return the URI of the loaded ontology
+	 */
+	public String getOntologyURL() {
 		String ontologyURI = null;
-		Iterator iter = model.listOntologies();
-		
+		Iterator iter = ontModel.listOntologies();
+
 		if(iter.hasNext()) {
 			Ontology onto = (Ontology) iter.next();
 			ontologyURI = onto.getURI();
-			System.out.println("Ontology URI = "+ ontologyURI);
-		} 
-		*/
-		
-		// retrive the properties of a specified class
-		/*
-		OntClass student = model.getOntClass("http://medicis/spm.owl/OntoSPM#patient");
-		
+			return "Ontology URI = "+ ontologyURI;
+		}
+		return null; 
+
+	}
+
+	/**
+	 * Method to get all the properties of a specific class
+	 * (to test and see what we have)
+	 * @param classURI
+	 * @return an array list of all the properties of a specific class
+	 */
+	public ArrayList<String> getPropertiesOfClass(String classURI) {
+		OntClass student = ontModel.getOntClass(classURI);
+		ArrayList<String> propAL = new ArrayList();
 		Iterator propIter = student.listDeclaredProperties();
 		while (propIter.hasNext()) {
 			OntProperty property = (OntProperty) propIter.next();
-			System.out.println(property.getLocalName());
+			propAL.add(property.getLocalName());
 		}
-		*/
-		
-		// list all datatype properties in the model
-		/*
-		Iterator iter = model.listDatatypeProperties();
+		return propAL;
+	}
+
+	/**
+	 * A method to print all the datatype properties present in the ontology to the console
+	 * Format: datatype property -> domain -> range
+	 */
+	public void getAllDatatypeProperties() {
+		Iterator iter = ontModel.listDatatypeProperties();
 		while (iter.hasNext()) {
 			DatatypeProperty prop = (DatatypeProperty) iter.next();
 			String propName = prop.getLocalName();
@@ -98,57 +131,66 @@ public class OntologyEditor {
 				rng = prop.getRange().getLocalName();
 			System.out.println(propName + ": \t (" + dom +") \t -> (" + rng+") ");
 		}
-		*/
-		
-		// List all individuals in the model
-		/*
-		Iterator individuals = model.listIndividuals();
-		while (individuals.hasNext()) {
-			Individual individual = (Individual) individuals.next();
-			System.out.println(individual.getLocalName());
-		}
-		*/
-		
-		
-		
-		// create an individual		
-		OntClass test = model.getOntClass("http://medicis/spm.owl/OntoSPM#patient");
-		Individual indv = test.createIndividual("http://www.semanticweb.org/ontologies/2021/1/24/IOMO/IOMO_0000263");
-		indv.addLabel("Patient 1", "EN");
-		
-		Property hasFirstName = model.getProperty("http://www.semanticweb.org/ontologies/2021/1/24/IOMO/IOMO_000025");
-		
-		indv.addProperty(hasFirstName, "Derek");
-	
-		// List all individuals in the model
-				
-				Iterator individuals = model.listIndividuals();
-				while (individuals.hasNext()) {
-					Individual individual = (Individual) individuals.next();
-					System.out.println(individual.getLocalName());
-				}
-				
-				
-				
-				//save the model to a file
-				StringWriter sw = new StringWriter();
-				model.write(sw, "RDF/XML-ABBREV");
-				String owlCode = sw.toString();
-				
-				
-				File file = new File("myModel.owl");
-				try {
-					FileWriter fw = new FileWriter(file);
-					fw.write(owlCode);
-					fw.close();
-				} catch (FileNotFoundException fnfe) {
-					fnfe.printStackTrace();
-				} catch (IOException ioe) {
-					ioe.printStackTrace();
-				}
-				
 	}
-	
 
+	/**
+	 * Method to get all the Individuals of the ontology
+	 * @return ArrayList of the individuals (label)
+	 */
+	public ArrayList<String> getAllIndividuals() {
+		ArrayList<String> indAL = new ArrayList();
+		Iterator indIter = ontModel.listIndividuals();
+		while (indIter.hasNext()) {
+			Individual ind = (Individual) indIter.next();
+			indAL.add(ind.getLabel("EN"));
+		}
+		return indAL;
+	}
+
+
+	/**
+	 * Method to create a new individual
+	 * @param classURI the class URI of the class where a new individual should be added
+	 * @param newIndvURI the URI of the new individual (has to be edited with counter!)
+	 * @param indvLabel the label of the new individual
+	 */
+	public void createNewOntIndividual(String classURI, String newIndvURI, String indvLabel) {
+		OntClass ontClass = ontModel.getOntClass(classURI);
+		Individual indv = ontClass.createIndividual(newIndvURI);
+		indv.addLabel(indvLabel, "EN");
+	}
+
+
+	/**
+	 * Method to add property to individual
+	 * @param indvURI the URI of the individual
+	 * @param propURI the URI of the property
+	 * @param name the name/age/number we want to add
+	 */
+	public void addPropertyToIndividual(String indvURI, String propURI, String name) {
+		Individual indv = ontModel.getIndividual(indvURI);
+		DatatypeProperty firstName = ontModel.getDatatypeProperty(propURI);
+		indv.addProperty(firstName, ontModel.createTypedLiteral(name));
+	}
+
+
+	/**
+	 * Method to save the edited ontology into an owl file
+	 */
+	public void saveNewOWLFile() {
+		StringWriter sw = new StringWriter();
+		ontModel.write(sw, "RDF/XML-ABBREV");
+		String owlCode = sw.toString();
+		File file = new File("myModel.owl");
+		try {
+			FileWriter fw = new FileWriter(file);
+			fw.write(owlCode);
+			fw.close();
+		} catch (FileNotFoundException fnfe) {
+			fnfe.printStackTrace();
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+
+	}
 }
-
