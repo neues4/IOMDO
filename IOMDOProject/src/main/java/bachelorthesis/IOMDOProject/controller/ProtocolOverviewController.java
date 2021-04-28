7d300ce718b56ada577173f76801395a2679262c
romap1 <56729339+romap1@users.noreply.github.com>
2021-04-28 11:44:09 +0200

    TableTreeView in TabelView umgeändert

--- IOMDOProject/src/main/java/bachelorthesis/IOMDOProject/controller/ProtocolOverviewController.java
+++ IOMDOProject/src/main/java/bachelorthesis/IOMDOProject/controller/ProtocolOverviewController.java
@@ -1,21 +1,22 @@
 package bachelorthesis.IOMDOProject.controller;
 
 import java.io.IOException;
-
-import javafx.beans.property.ReadOnlyStringWrapper;
+import bachelorthesis.IOMDOProject.model.OntologyEditor;
+import bachelorthesis.IOMDOProject.model.Patient;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeTableCell;
-import javafx.scene.control.TreeTableColumn;
-import javafx.scene.control.TreeTableColumn.CellDataFeatures;
-import javafx.scene.control.TreeTableView;
-import javafx.scene.layout.GridPane;
-import javafx.util.Callback;
 
-public class ProtocolOverviewController {
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.GridPane;
+
+
+public class ProtocolOverviewController  {
 
 	@FXML
 	private GridPane gridPaneProtocolOverview;
@@ -26,40 +27,45 @@ public class ProtocolOverviewController {
 	@FXML
 	private Button searchBtn;
 	
-	@FXML
-	private TreeTableView<String> ProtocolTTView;
 	
 	@FXML
-	private TreeTableColumn<String, String> surnameTTColumn;
+	private TableView<Patient> protocolTblView;
 	
+	@FXML
+	private TableColumn<Patient, String> surnameTblColumn;
 	
+	@FXML
+	private TableColumn<Patient, String> firstnameTblColumn;
 	
-	TreeItem<String> surname = new TreeItem<>("Tidus");
+	 @FXML
+	    public void initialize() {
+		 
+			surnameTblColumn.setCellValueFactory(new PropertyValueFactory<Patient, String>("surname"));
+			firstnameTblColumn.setCellValueFactory(new PropertyValueFactory<Patient, String>("firstname"));
+			//protocolTblView.setPlaceholder(new Label("placeholder"));
+			protocolTblView.setItems(list);
+	 }
+	
+	OntologyEditor oe = new OntologyEditor("C:\\Users\\patty\\Desktop\\IOMO_19.owl");
+
+	
+	public ObservableList<Patient> list = FXCollections.observableArrayList(
+			new Patient("Tidus", "Test"),
+			new Patient("Yuna", "West"));
+	
 	
 	public void test(ActionEvent event) throws IOException{
-	
-		TreeItem<String> childNode1 = new TreeItem<>("Child Node 1");
-		TreeItem<String> root = new TreeItem<>("Root node");
-		root.getChildren().setAll(childNode1);    
+		oe.getPropertyOfIndividual("http://www.semanticweb.org/ontologies/2021/1/24/IOMO/IOMO_0000262", "http://www.semanticweb.org/ontologies/2021/1/24/IOMO/IOMO_253");
+		//surnameTblColumn.setCellValueFactory(new PropertyValueFactory<Patient, String>("surname"));
+		//firstnameTblColumn.setCellValueFactory(new PropertyValueFactory<Patient, String>("firstname"));
+		//protocolTblView.setPlaceholder(new Label("placeholder"));
+		//protocolTblView.setItems(list);
+		//System.out.println(oe.getOntologyClass("http://medicis/spm.owl/OntoSPM#patient"));
+		//System.out.println(oe.getAllIndividuals());
 		
-		surnameTTColumn.setCellValueFactory((CellDataFeatures<String, String> p) ->
-		new ReadOnlyStringWrapper(p.getValue().getValue())); 
-	
-		 ProtocolTTView.setRoot(root);
-		
-	/**
-		surnameTTColumn.setCellFactory(new Callback<TreeTableColumn<String,String>, TreeTableCell<String,String>>() {
-			
-			@Override
-			public TreeTableCell<String, String> call(TreeTableColumn<String, String> param) {
-				// TODO Auto-generated method stub
-				return null;
-			}
-		});
-		*/
 		
 	}
 	
-	
-	
+
 }
+
