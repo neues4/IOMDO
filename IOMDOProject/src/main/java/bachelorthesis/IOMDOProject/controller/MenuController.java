0e8a8f4bd13d89bf63a6d9577b92407391c14a73
romap1 <56729339+romap1@users.noreply.github.com>
2021-05-18 17:43:02 +0200

    Record document navigation

--- IOMDOProject/src/main/java/bachelorthesis/IOMDOProject/controller/MenuController.java
+++ IOMDOProject/src/main/java/bachelorthesis/IOMDOProject/controller/MenuController.java
@@ -34,10 +34,15 @@ public class MenuController implements Initializable {
 	@FXML
 	private Button logOutBtn;
 	@FXML
-	private Button test;
+	private Button saveBtn;
+	
 	// ÜBERGANGSLÖSUNG
 	@FXML
 	private Button baselinesBtn;
+	@FXML
+	private Button patientDataBtn;
+	@FXML
+	private Button iomDocumentationBtn;
 	
 	@FXML
 	private Label userDisplay;
@@ -58,9 +63,14 @@ public class MenuController implements Initializable {
 	}
 
 	//übergangslösung für verknüpfung zuIOMDocumentation.fxml
-	public  void test(ActionEvent event) throws IOException {
+	public  void openIOMDocumentation(ActionEvent event) throws IOException {
 		Parent p = m.returnParent("IOMDocumentation.fxml");
 		borderPaneMenu.setCenter(p);
+		baselinesBtn.setDisable(false);
+		patientDataBtn.setDisable(true);
+		iomDocumentationBtn.setDisable(false);
+		saveBtn.setDisable(false);
+		
 	}
 
 	public void openNewProtocol(ActionEvent event) throws IOException {
@@ -69,12 +79,24 @@ public class MenuController implements Initializable {
 		borderPaneMenu.setCenter(p);
 		//gridPane.getChildren().removeAll();	
 		//gridPane.getChildren().setAll(p);
+		baselinesBtn.setVisible(true);
+		patientDataBtn.setVisible(true);
+		iomDocumentationBtn.setVisible(true);
+		saveBtn.setVisible(true);
+		baselinesBtn.setDisable(false);
+		patientDataBtn.setDisable(true);
+		iomDocumentationBtn.setDisable(true);
+		saveBtn.setDisable(true);
 	}
 
 	public  void openProtocolOverview(ActionEvent event) throws IOException {
 		//Main m = new Main();
 		Parent p = m.returnParent("ProtocolOverview.fxml");
 		borderPaneMenu.setCenter(p);
+		baselinesBtn.setVisible(false);
+		patientDataBtn.setVisible(false);
+		iomDocumentationBtn.setVisible(false);
+		saveBtn.setVisible(false);
 
 		//borderPaneMenu.setCenter(p);
 		//BorderPane bp = (BorderPane) this.borderPaneMenu;
@@ -94,8 +116,24 @@ public class MenuController implements Initializable {
 	public void openBaselines(ActionEvent event) throws IOException {
 		Parent p = m.returnParent("Baselines.fxml");
 		borderPaneMenu.setCenter(p);
+		patientDataBtn.setDisable(false);
+		iomDocumentationBtn.setDisable(false);
+		saveBtn.setDisable(true);
+		
 	}
+	
+	public void openPatientData(ActionEvent event) throws IOException {
+		Parent p = m.returnParent("RecordDocument_PatientData.fxml");
+		borderPaneMenu.setCenter(p);
+		iomDocumentationBtn.setDisable(true);
+		}
+	
+	
 
+	public void saveDocumentation(ActionEvent event) throws IOException {
+		baselinesBtn.setDisable(true);
+	}
+	
 	/**
 	 * 
 	 * @param event
