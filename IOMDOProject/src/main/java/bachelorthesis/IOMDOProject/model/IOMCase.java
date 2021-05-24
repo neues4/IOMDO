e83c2fe697ccd4c03e634b91402b90c41fafe8dc
romap1 <56729339+romap1@users.noreply.github.com>
2021-05-24 18:38:55 +0200

    IOMUseCase added

---
+++ IOMDOProject/src/main/java/bachelorthesis/IOMDOProject/model/IOMCase.java
@@ -0,0 +1,65 @@
+package bachelorthesis.IOMDOProject.model;
+
+import java.time.LocalDate;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class IOMCase {
+
+	
+	//Property --> automatische updates möglich
+		private SimpleStringProperty surname;
+		private SimpleStringProperty firstname;
+		private SimpleStringProperty PID;
+		private SimpleStringProperty FID;
+		private SimpleStringProperty birthday;
+		private SimpleIntegerProperty caseNr;
+		//private ObjectProperty <LocalDate>birthday;
+		
+		/**
+		 * 
+		 * @param surname
+		 * @param firstname
+		 * @param birthday
+		 * @param PID
+		 * @param FID
+		 */
+		public IOMCase(String surname, String firstname, String birthday, String PID, String FID, int caseNr) {
+			this.surname = new SimpleStringProperty(surname);
+			this.firstname = new SimpleStringProperty(firstname);
+			//this.birthday = new SimpleObjectProperty<>(birthday);
+			this.birthday = new SimpleStringProperty(birthday);
+			this.PID = new SimpleStringProperty(PID);
+			this.FID = new SimpleStringProperty(FID);
+			this.caseNr = new SimpleIntegerProperty(caseNr);
+		}
+		
+		public String getSurname() {
+			return surname.get();
+		}
+		public String getFirstname() {
+			return firstname.get();
+		}
+		//public LocalDate getbirthday() {
+			//return birthday.get();
+		//}
+		public String getPID() {
+			return PID.get();
+		}
+		public String getFID() {
+			return FID.get();
+		}
+		public String getBirthday() {
+			return birthday.get();
+		}
+		public int getCaseNr() {
+			return caseNr.get();
+		}
+		
+		//public ObjectProperty<LocalDate> birthdayProperty() {
+		  //  return birthday;
+		//}
+	
+}
