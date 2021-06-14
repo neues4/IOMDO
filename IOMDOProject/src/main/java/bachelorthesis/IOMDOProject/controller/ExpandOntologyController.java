13078f51d7a4dff65030d958ca4d812625c031ca
Stefanie Neuenschwander <stefanie.neuenschwander97@gmail.com>
2021-06-14 22:17:15 +0200

    added webprotege

--- IOMDOProject/src/main/java/bachelorthesis/IOMDOProject/controller/ExpandOntologyController.java
+++ IOMDOProject/src/main/java/bachelorthesis/IOMDOProject/controller/ExpandOntologyController.java
@@ -3,6 +3,8 @@ package bachelorthesis.IOMDOProject.controller;
 import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -14,8 +16,8 @@ public class ExpandOntologyController {
 	private Button btnProtege;
 
 	@FXML
-	void openProtege(ActionEvent event) throws IOException {
-
+	void openProtege(ActionEvent event) throws IOException, URISyntaxException {
+/*
 		// mac
 		File file = new File("/Users/stefanie/Desktop/exceptionTest.owl");
 		// windows
@@ -23,6 +25,8 @@ public class ExpandOntologyController {
 		if (Desktop.isDesktopSupported()) {
 			Desktop.getDesktop().open(file);
 
-		}
+		}*/
+		
+		Desktop.getDesktop().browse(new URL("https://webprotege.stanford.edu/#projects/4ff1be76-81df-425e-b567-99996c664c9f/edit/Classes?selection=Class(%3Chttp://purl.obolibrary.org/obo/BFO_0000002%3E)").toURI());
 	}
 }
