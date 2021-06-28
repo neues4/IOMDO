fe84ec91707877ae8f8568235a412d4dbe15b152
romap1 <56729339+romap1@users.noreply.github.com>
2021-06-28 11:06:02 +0200

    Final Ontology

--- IOMDOProject/src/main/java/bachelorthesis/IOMDOProject/model/OntologyEditor.java
+++ IOMDOProject/src/main/java/bachelorthesis/IOMDOProject/model/OntologyEditor.java
@@ -76,7 +76,7 @@ public class OntologyEditor {
 		
 		if (editor == null)
 			//Windows
-			editor = new OntologyEditor("src\\\\main\\\\resources\\\\bachelorthesis\\\\IOMDOProject\\\\IOMO_40.owl");
+			editor = new OntologyEditor("myModel.owl");//"src\\\\main\\\\resources\\\\bachelorthesis\\\\IOMDOProject\\\\IOMO_FINAL_withoutDepecrated.owl");
 		//mac
 		//editor = new OntologyEditor("/Users/stefanie/Documents/maven.1619428611109/IOMDOProject/src/main/resources/bachelorthesis/IOMDOProject/IOMO_40.owl");
 
